35c8ad711f7b12a8150997ddadf2461bf3a42899
venkee77 <venkee77@gmail.com>
2015-04-19 11:28:44 -0700

    second c commit

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 var Hapi = require("hapi");
 var server = new Hapi.Server();
-var Path = require('path');
+var Path = require("path");
+var boom = require('boom');
 
 server.connection({port:3000});
 
-server.register(require('hapi-auth-cookie'),function(err){
-  server.auth.strategy('session','cookie',{
-    password: "none",
-    cookie: "hapi-samples",
-    redirectTo: "/index",
-    isSecure: false
-  });
-});
+server.register(
+    [
+      {
+        register:require('hapi-auth-cookie')
+      },
+      {
+        register: require('./plugins/Routes')
+      }
+/*      ,
+      {
+        register: require('./plugins/Views')
+      }*/
+    ],function(err){
+      if(!err){
+        server.auth.strategy('session','cookie',{
+          password: "none",
+          cookie: "hapi-samples",
+          redirectTo: "/index",
+          isSecure: false
+        });
+      }
+    }
+);
 
 server.views({
     engines:{
@@ -23,42 +39,7 @@ server.views({
     partialsPath: Path.join(__dirname,'views/partials')
 });
 
-server.route([
-  { method: 'GET',  path: '/', config:{  handler: function(request,reply){
-                                                    reply.view('index');
-                                                  },
-                                       plugins:{
-                                         'hapi-auth-cookie':{
-                                           redirectTo: false
-                                         }
-                                       }
-                                     }
-  },
-  { method: 'GET',  path: '/home',config:{  handler: function(request,reply){
-                                                    console.log('home');
-                                                    reply.view('home');
-                                                  },
-                                           auth:'session'
-                                    }
-  },
-  { method: 'POST',  path: '/signin', config:{  handler: function(request,reply){
-                                                    var postData = request.payload;
 
-                                                    if(postData["username"] === postData["password"] && postData["username"] !== ""){
-                                                      request.auth.session.set({username:postData["username"]});
-                                                      console.log(request.auth.credentials);
-                                                    }
-
-                                                    reply.view('home',{username:postData["username"]});
-                                                  },
-                                       plugins:{
-                                         'hapi-auth-cookie':{
-                                           redirectTo: false
-                                         }
-                                       }
-                                     }
-  }
-
-  ]);
+//server["views"].addViewManager();
 
 server.start();
